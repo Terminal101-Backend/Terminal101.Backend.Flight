0925ab56c5557106bc38443004b18cee32cc3c14
Samir <mail6410@gmail.com>
2022-01-24 17:29:26 +0000

    Add travel class enum

---
+++ app/constants/ETravelClass.js
@@ -0,0 +1,17 @@
+const Enum = require("../core/enum");
+const EEnumType = require("./EEnumType");
+
+class EFlightWaypoint extends Enum {
+  constructor() {
+    super(EEnumType.SYMBOLIC);
+
+    this.add("ECONOMY");
+    this.add("PERMIUM_ECONOMY");
+    this.add("BUSINESS");
+    this.add("FIRST");
+
+    this.freeze();
+  }
+}
+
+module.exports = new EFlightWaypoint();
