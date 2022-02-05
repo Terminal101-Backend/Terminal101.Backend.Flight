af5bd7914d17f04ed62329cc926dc2eb8a72d31d
Samir <mail6410@gmail.com>
2022-02-05 15:39:41 +0000

    Remove strange codesgit diff package.json

--- app/repositories/flightInfoRepository.js
+++ app/repositories/flightInfoRepository.js
@@ -164,7 +164,6 @@ class FlightInfoRepository extends BaseRepository {
    * @returns {Promise<String>}
    */
   async generateUniqueCode(length = 20) {
-    RRRRE
     let code;
     while (!code) {
       code = generateRandomString(length, length, true, true, true);
