ccc72742959c4d2537b2dfedf92698243e2f7f84
Samir <mail6410@gmail.com>
2022-05-14 21:45:35 +0000

    Add CreateSession and AirLowFareSearch methods for parto service

--- app/services/index.js
+++ app/services/index.js
@@ -1,5 +1,6 @@
 module.exports.accountManagement = require("./accountManagement");
 module.exports.amadeus = require("./amadeus");
+module.exports.parto = require("./parto");
 module.exports.wallet = require("./wallet");
 module.exports.common = require("./common");
 module.exports.ip = require("./ip");
