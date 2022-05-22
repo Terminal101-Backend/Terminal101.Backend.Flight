490eab7e5a05c028631ed2f5cc576ef969eef2dd
Samir <mail6410@gmail.com>
2022-05-22 20:27:45 +0000

    Update

--- app/routes/tempRoute.js
+++ app/routes/tempRoute.js
@@ -26,7 +26,8 @@ router.get("/socket", (req, res) => {
   <script>
     var socket;
     window.onload = () => {
-      socket = io("https://test-terminal101-flight.herokuapp.com/", {cors: {withCredentials: true}});
+      // socket = io("https://test-terminal101-flight.herokuapp.com/", {cors: {withCredentials: true}});
+      socket = io("http://flight.terminal101.co/", {cors: {withCredentials: true}});
       // socket = io();
   
       socket.on("searchFlightResult", result => {
