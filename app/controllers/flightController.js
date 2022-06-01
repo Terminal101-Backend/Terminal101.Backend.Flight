50f70da60a49c6673231661e8a83d95082d168ab
Samir <mail6410@gmail.com>
2022-06-01 09:48:21 +0000

    Update

--- app/controllers/flightController.js
+++ app/controllers/flightController.js
@@ -18,8 +18,8 @@ module.exports.searchOriginDestination = async (req, res) => {
       if (EFlightWaypoint.check("ORIGIN", req.params.waypointType)) {
         const ip = request.getRequestIpAddress(req);
         ipInfo = await getIpInfo(ip);
+        // ipInfo = await getIpInfo("5.239.149.82");
         console.log({ ip, ipInfo });
-        // const ipInfo = await getIpInfo("24.48.0.1");
         if (ipInfo.status === "success") {
           keyword = ipInfo.city;
         }
