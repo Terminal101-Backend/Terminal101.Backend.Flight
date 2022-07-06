86a082d0f530a3acca5a965bb5fe65b8dc99e8b2
Samir <mail6410@gmail.com>
2022-07-06 13:17:37 +0000

    Remove unwanted methods

--- app/services/amadeusSoap.js
+++ app/services/amadeusSoap.js
@@ -5,9 +5,9 @@ let accessToken = "";
 // Request interceptor for API calls
 axiosApiInstance.interceptors.request.use(
   async config => {
-    config.baseURL = process.env.AMADEUS_BASE_URL;
+    config.baseURL = process.env.AMADEUS_SERVICE_BASE_URL;
     config.headers = {
-      'Authorization': `Bearer ${accessToken}`,
+      // 'Authorization': `Bearer ${accessToken}`,
       'Accept': 'application/json',
       // 'Content-Type': 'application/x-www-form-urlencoded',
       'Content-Type': 'application/json',
@@ -19,89 +19,41 @@ axiosApiInstance.interceptors.request.use(
   });
 
 // Response interceptor for API calls
-axiosApiInstance.interceptors.response.use((response) => {
-  return response
-}, async function (error) {
-  const originalRequest = error.config;
+// axiosApiInstance.interceptors.response.use((response) => {
+//   return response
+// }, async function (error) {
+//   const originalRequest = error.config;
 
-  if (!!error.response && [401, 403].includes(error.response.status) && !originalRequest._retry) {
-    originalRequest._retry = true;
-    await getAccessToken();
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + accessToken;
-    return axiosApiInstance(originalRequest);
-  }
-  return Promise.reject(error);
-});
+//   if (!!error.response && [401, 403].includes(error.response.status) && !originalRequest._retry) {
+//     originalRequest._retry = true;
+//     await getAccessToken();
+//     axios.defaults.headers.common['Authorization'] = 'Bearer ' + accessToken;
+//     return axiosApiInstance(originalRequest);
+//   }
+//   return Promise.reject(error);
+// });
 
-const getAccessToken = async () => {
-  const params = new URLSearchParams();
-  params.append("grant_type", "client_credentials");
-  params.append("client_id", process.env.AMADEUS_API_KEY);
-  params.append("client_secret", process.env.AMADEUS_API_SECRET);
-  delete axios.defaults.headers.common['Authorization'];
+// const getAccessToken = async () => {
+//   const params = new URLSearchParams();
+//   params.append("grant_type", "client_credentials");
+//   params.append("client_id", process.env.AMADEUS_API_KEY);
+//   params.append("client_secret", process.env.AMADEUS_API_SECRET);
+//   delete axios.defaults.headers.common['Authorization'];
 
-  const {
-    data: response
-  } = await axios.post(process.env.AMADEUS_BASE_URL + "/v1/security/oauth2/token", params, {
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded'
-    },
-  });
+//   const {
+//     data: response
+//   } = await axios.post(process.env.AMADEUS_BASE_URL + "/v1/security/oauth2/token", params, {
+//     headers: {
+//       'content-type': 'application/x-www-form-urlencoded'
+//     },
+//   });
 
-  accessToken = response.access_token;
+//   accessToken = response.access_token;
 
-  return response;
-};
+//   return response;
+// };
 
-const airlineCodeLookup = async code => {
-  const {
-    data: response
-  } = await axiosApiInstance.get("/v1/reference-data/airlines", {
-    params: {
-      airlineCodes: code
-    }
-  });
-
-  return response;
-};
-
-const searchAirportAndCity = async keyword => {
-  const {
-    data: response
-  } = await axiosApiInstance.get("/v1/reference-data/locations", {
-    params: {
-      subType: "AIRPORT,CITY",
-      keyword
-    },
-  });
-
-  return response;
-};
-
-const flightOffersSingleSearch = async (originLocationCode, destinationLocationCode, departureDate, returnDate, adults = 1, children, infants, travelClass, includedAirlineCodes, excludedAirlineCodes, nonStop, currencyCode = "USD") => {
-  const {
-    data: response
-  } = await axiosApiInstance.get("/v2/shopping/flight-offers", {
-    params: {
-      originLocationCode,
-      destinationLocationCode,
-      departureDate,
-      returnDate,
-      adults,
-      children,
-      infants,
-      travelClass,
-      // includedAirlineCodes,
-      // excludedAirlineCodes,
-      // nonStop,
-      currencyCode,
-    },
-  });
-
-  return response;
-};
-
-const flightOffersMultiSearch = async (originLocationCode, destinationLocationCode, departureDate, returnDate, segments, adults = 1, children, infants, travelClass, includedAirlineCodes, excludedAirlineCodes, nonStop, currencyCode = "USD") => {
+const searchFlight = async (originLocationCode, destinationLocationCode, departureDate, returnDate, segments, adults = 1, children, infants, travelClass, includedAirlineCodes, excludedAirlineCodes, nonStop, currencyCode = "USD") => {
   const originDestinations = [];
   originDestinations.push({
     id: 0,
@@ -169,33 +121,7 @@ const flightOffersMultiSearch = async (originLocationCode, destinationLocationCo
   return response;
 };
 
-const covid19AreaReport = async (countryCode, cityCode) => {
-  const {
-    data: response
-  } = await axiosApiInstance.get("/v1/duty-of-care/diseases/covid19-area-report", {
-    params: {
-      countryCode,
-      cityCode,
-    },
-  });
-
-  return response;
-};
-
-const updateFlightPrice = async flightOffer => {
-  const {
-    data: response
-  } = await axiosApiInstance.post("/v1/shopping/flight-offers/pricing", {
-    data: {
-      type: "flight-offers-pricing",
-      flightOffers: [flightOffer]
-    }
-  });
-
-  return response;
-};
-
-const flightCreateOrder = async (flightOffer, travelers) => {
+const bookFlight = async (flightOffer, travelers) => {
   const {
     data: response
   } = await axiosApiInstance.post("/v1/booking/flight-orders", {
@@ -209,31 +135,8 @@ const flightCreateOrder = async (flightOffer, travelers) => {
   return response;
 };
 
-const getFlightOrder = async (orderId) => {
-  const {
-    data: response
-  } = await axiosApiInstance.get(`/v1/booking/flight-orders/${orderId}`);
-
-  return response;
-};
-
-const deleteFlightOrder = async (orderId) => {
-  const {
-    data: response
-  } = await axiosApiInstance.delete(`/v1/booking/flight-orders/${orderId}`);
-
-  return response;
-};
 
 module.exports = {
-  getAccessToken,
-  airlineCodeLookup,
-  searchAirportAndCity,
-  flightOffersSingleSearch,
-  flightOffersMultiSearch,
-  updateFlightPrice,
-  covid19AreaReport,
-  flightCreateOrder,
-  getFlightOrder,
-  deleteFlightOrder
+  searchFlight,
+  bookFlight,
 };
