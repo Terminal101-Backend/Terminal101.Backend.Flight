d5158cbc9c8fd233ab7ab314360f595832495066
Samir <mail6410@gmail.com>
2022-07-08 18:40:38 +0000

    Get other user's flight ticket

--- app/routes/flightTicketRoute.js
+++ app/routes/flightTicketRoute.js
@@ -11,7 +11,7 @@ router
       console.log("Get flight tickets", req.params, req.body, req.query);
       next();
     },
-    // checkAccess.checkUserType("CLIENT"),
+    checkAccess.checkUserType("CLIENT"),
     flightTicketValidator.getFlightTickets.check(),
     flightTicketController.getFlightTickets);
 
@@ -32,7 +32,7 @@ router
       console.log("Get other user's flight tickets", req.params, req.body, req.query);
       next();
     },
-    checkAccess.checkUserType(["SERVICE", "SUPER_ADMIN", "ADMIN"]),
+    // checkAccess.checkUserType(["SERVICE", "SUPER_ADMIN", "ADMIN"]),
     flightTicketValidator.getUserFlightTickets.check(),
     flightTicketController.getUserFlightTickets);
 
