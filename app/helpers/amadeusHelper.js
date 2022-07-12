4db6cb8bf9a77edd1e5271b2b520975b0a09ae42
Samir <mail6410@gmail.com>
2022-07-12 17:52:30 +0000

    Convert Amadeus result to Terminal101 model

--- app/helpers/amadeusHelper.js
+++ app/helpers/amadeusHelper.js
@@ -1,6 +1,6 @@
 const dateTimeHelper = require("./dateTimeHelper");
 const { amadeus, amadeusSoap } = require("../services");
-const { countryRepository, flightInfoRepository } = require("../repositories");
+const { countryRepository, flightInfoRepository, airlineRepository } = require("../repositories");
 const { EProvider } = require("../constants");
 
 const makeSegmentsArray = segments => {
@@ -48,51 +48,48 @@ const makeSegmentStopsArray = airports => {
 const makeFlightSegmentsArray = (aircrafts, airlines, airports) => {
   return segment => {
     let result = {
-      duration: dateTimeHelper.convertAmadeusTime(segment.duration),
-      flightNumber: segment.number,
-      aircraft: aircrafts[segment.aircraft.code],
-      airline: {
-        code: segment.carrierCode,
-        name: airlines[segment.carrierCode],
-      },
+      duration: dateTimeHelper.convertAmadeusTime(segment.flightDetail.flightDuration),
+      flightNumber: segment.marketingCarrier.flightNumber,
+      aircraft: aircrafts[segment.equipment.aircraftCode],
+      airline: airlines[segment.marketingCarrier.airlineID],
       stops: (segment.stops ?? []).map(makeSegmentStopsArray(airports)),
       departure: {
-        airport: !!airports[segment.departure.iataCode] ? airports[segment.departure.iataCode].airport : {
-          code: segment.departure.iataCode,
+        airport: !!airports[segment.originDestination.departure.airportCode] ? airports[segment.originDestination.departure.airportCode].airport : {
+          code: segment.originDestination.departure.airportCode,
           name: "UNKNOWN"
         },
-        city: !!airports[segment.departure.iataCode] ? airports[segment.departure.iataCode].city : {
+        city: !!airports[segment.originDestination.departure.airportCode] ? airports[segment.originDestination.departure.airportCode].city : {
           code: "UNKNOWN",
           name: "UNKNOWN"
         },
-        country: !!airports[segment.departure.iataCode] ? airports[segment.departure.iataCode].country : {
+        country: !!airports[segment.originDestination.departure.airportCode] ? airports[segment.originDestination.departure.airportCode].country : {
           code: "UNKNOWN",
           name: "UNKNOWN"
         },
-        terminal: segment.departure.terminal,
-        at: segment.departure.at,
+        terminal: segment.originDestination.departure.terminalName,
+        at: segment.originDestination.departure.date + "T" + segment.originDestination.departure.time,
       },
       arrival: {
-        airport: !!airports[segment.arrival.iataCode] ? airports[segment.arrival.iataCode].airport : {
-          code: segment.arrival.iataCode,
+        airport: !!airports[segment.originDestination.arrival.airportCode] ? airports[segment.originDestination.arrival.airportCode].airport : {
+          code: segment.originDestination.arrival.airportCode,
           name: "UNKNOWN"
         },
-        city: !!airports[segment.arrival.iataCode] ? !!airports[segment.arrival.iataCode] ? airports[segment.arrival.iataCode].city : {
-          code: segment.arrival.iataCode,
+        city: !!airports[segment.originDestination.arrival.airportCode] ? !!airports[segment.originDestination.arrival.airportCode] ? airports[segment.originDestination.arrival.airportCode].city : {
+          code: segment.originDestination.arrival.airportCode,
           name: "UNKNOWN"
         } : {
           code: "UNKNOWN",
           name: "UNKNOWN"
         },
-        country: !!airports[segment.arrival.iataCode] ? !!airports[segment.arrival.iataCode] ? airports[segment.arrival.iataCode].country : {
-          code: segment.arrival.iataCode,
+        country: !!airports[segment.originDestination.arrival.airportCode] ? !!airports[segment.originDestination.arrival.airportCode] ? airports[segment.originDestination.arrival.airportCode].country : {
+          code: segment.originDestination.arrival.airportCode,
           name: "UNKNOWN"
         } : {
           code: "UNKNOWN",
           name: "UNKNOWN"
         },
-        terminal: segment.arrival.terminal,
-        at: segment.arrival.at,
+        terminal: segment.originDestination.arrival.terminalName,
+        at: segment.originDestination.arrival.date + "T" + segment.originDestination.arrival.time,
       },
     };
 
@@ -101,31 +98,30 @@ const makeFlightSegmentsArray = (aircrafts, airlines, airports) => {
 };
 
 const makePriceObject = (flightPrice, travelerPricings) => ({
-  total: parseFloat(flightPrice.total),
-  grandTotal: parseFloat(flightPrice.grandTotal),
-  base: parseFloat(flightPrice.base),
-  fees: (flightPrice.fees ?? []).map(fee => ({
-    amount: parseFloat(fee.amount),
-    type: fee.type,
-  })),
-  taxes: (flightPrice.taxes ?? []).map(tax => ({
-    amount: parseFloat(tax.amount),
-    code: tax.code,
-  })),
+  total: parseFloat(flightPrice.totalPrice),
+  grandTotal: parseFloat(flightPrice.totalPrice),
+  base: travelerPricings.reduce((total, travelerPrice) => total + parseFloat(travelerPrice.baseAmount), 0),
+  fees: [],
+  taxes: [
+    {
+      amount: travelerPricings.reduce((total, travelerPrice) => total + parseFloat(travelerPrice.taxesAmount), 0),
+      code: "Tax",
+    }
+  ],
   travelerPrices: travelerPricings.map(travelerPrice => {
     let travelerType;
-    switch (travelerPrice.travelerType) {
-      case "CHILD":
+    switch (travelerPrice.passengerType) {
+      case "CHD":
         travelerType = "CHILD";
         break;
 
-      case "HELD_INFANT":
-      case "SEATED_INFANT":
+      case "INF":
+        // case "SEATED_INFANT":
         travelerType = "INFANT";
         break;
 
-      case "ADULT":
-      case "SENIOR":
+      case "ADT":
+        // case "SENIOR":
         travelerType = "ADULT";
         break;
 
@@ -133,17 +129,14 @@ const makePriceObject = (flightPrice, travelerPricings) => ({
     }
 
     return {
-      total: parseFloat(travelerPrice.price.total),
-      base: parseFloat(travelerPrice.price.base),
+      total: parseFloat(travelerPrice.baseAmount + travelerPrice.taxesAmount),
+      base: parseFloat(travelerPrice.baseAmount),
       travelerType,
-      fees: (travelerPrice.price.fees ?? []).map(fee => ({
-        amount: parseFloat(fee.amount),
-        type: fee.type,
-      })),
-      taxes: (travelerPrice.price.taxes ?? []).map(tax => ({
-        amount: parseFloat(tax.amount),
-        code: tax.code,
-      })),
+      fees: [],
+      taxes: {
+        amount: parseFloat(travelerPrice.taxesAmount),
+        code: "Tax",
+      },
     }
   }),
 });
@@ -152,15 +145,20 @@ const makeFlightDetailsArray = (aircrafts, airlines, airports, travelClass = "EC
   return (flight, index) => {
     result = {
       code: `AMD-${index}`,
-      availableSeats: flight.numberOfBookableSeats,
+      availableSeats: 0,
       currencyCode: flight.price.currency,
       travelClass,
       provider: EProvider.get("AMADEUS"),
-      price: makePriceObject(flight.price, flight.travelerPricings),
-      itineraries: flight.itineraries.map(itinerary => {
+      providerData: {
+        offerId: flight.offerID,
+      },
+      price: makePriceObject(flight.price, flight.price.offerPrices),
+      itineraries: flight.flights.map(itinerary => {
+        const segments = itinerary.flightSegments.map(makeFlightSegmentsArray(aircrafts, airlines, airports));
+        const duration = segments.reduce((total, segment) => total + segment.duration, 0);
         let result = {
-          duration: dateTimeHelper.convertAmadeusTime(itinerary.duration),
-          segments: itinerary.segments.map(makeFlightSegmentsArray(aircrafts, airlines, airports)),
+          duration,
+          segments,
         };
 
         return result;
@@ -247,17 +245,33 @@ module.exports.searchFlights = async params => {
     };
   }
 
-  const stops = amadeusSearchResult
-    .reduce((res, cur) => [...res, ...cur.itineraries], [])
-    .reduce((res, cur) => [...res, ...cur.segments], [])
-    .filter(segment => !!segment.stops)
-    .reduce((res, cur) => [...res, ...cur.stops], [])
-    .map(stop => stop.iataCode);
+  // const stops = amadeusSearchResult
+  //   .reduce((res, cur) => [...res, ...cur.itineraries], [])
+  //   .reduce((res, cur) => [...res, ...cur.segments], [])
+  //   .filter(segment => !!segment.stops)
+  //   .reduce((res, cur) => [...res, ...cur.stops], [])
+  //   .map(stop => stop.iataCode);
 
-  const airports = !!amadeusSearchResult.dictionaries ? await countryRepository.getAirportsByCode([...Object.keys(amadeusSearchResult.dictionaries.locations), ...stops]) : [];
-  const aircrafts = !!amadeusSearchResult.dictionaries ? amadeusSearchResult.dictionaries.aircraft : [];
-  const carriers = !!amadeusSearchResult.dictionaries ? amadeusSearchResult.dictionaries.carriers : [];
-  const flightDetails = amadeusSearchResult.map(makeFlightDetailsArray(aircrafts, carriers, airports, params.travelClass));
+  const airportCodes = {};
+  const aircraftCodes = {};
+  const carrierCodes = {};
+
+  amadeusSearchResult.flights.forEach(flight => {
+    flight.flights.forEach(details => {
+      details.flightSegments.forEach(segment => {
+        airportCodes[segment.originDestination.departure.airportCode] = segment.originDestination.departure.airportCode;
+        airportCodes[segment.originDestination.arrival.airportCode] = segment.originDestination.arrival.airportCode;
+        carrierCodes[segment.marketingCarrier.airlineID] = segment.marketingCarrier.airlineID;
+        aircraftCodes[segment.equipment.aircraftCode] = segment.equipment.aircraftCode;
+      });
+    });
+  });
+
+  const airports = await countryRepository.getAirportsByCode(Object.keys(airportCodes));
+  const aircrafts = Object.keys(aircraftCodes);
+  const carriers = await airlineRepository.getAirlinesByCode(Object.keys(carrierCodes));
+
+  const flightDetails = amadeusSearchResult.flights.map(makeFlightDetailsArray(aircrafts, carriers, airports, params.travelClass));
 
   let origin = await countryRepository.getCityByCode(params.origin);
   let destination = await countryRepository.getCityByCode(params.destination);
