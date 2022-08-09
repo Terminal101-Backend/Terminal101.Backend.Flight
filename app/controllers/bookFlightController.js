90b24c36031750399c5748592369042f4fdf4bce
Samir <mail6410@gmail.com>
2022-08-09 07:32:08 +0000

    Update

--- app/controllers/bookFlightController.js
+++ app/controllers/bookFlightController.js
@@ -30,6 +30,7 @@ const pay = async (bookedFlight) => {
 
     bookedFlight.statuses.push({
       status: "INPROGRESS",
+      description: "Paid",
       changedBy: bookedFlight.bookedBy,
     });
 
@@ -147,7 +148,7 @@ module.exports.bookFlight = async (req, res) => {
     // TODO: Check if the user has not booked similar flight
     // const existsBookedFlight = await bookedFlightRepository.getDuplicatedBookedFlight(req.body.passengers, flightDetails.flights.itinerarry);
     const existsBookedFlight = await bookedFlightRepository.findOne({ bookedBy: decodedToken.user, searchedFlightCode: req.body.searchedFlightCode, flightDetailsCode: req.body.flightDetailsCode });
-    if (!!existsBookedFlight) {
+    if (!!existsBookedFlight && !EBookedFlightStatus.check(["CANCEL", "REJECTED"], existsBookedFlight.statuses[existsBookedFlight.statuses.length - 1].status)) {
       response.error(res, "booked_flight_duplicated", 406);
       return;
     }
