28f22915e4b37f2d82b13796a39141910306f64c
Samir <mail6410@gmail.com>
2022-09-11 22:49:40 +0430

    Update error handler

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+process.on("uncaughtException", function (err) {
+  let time = new Date().toString();
+  console.trace(`At ${time}`, err);
+});
+
 const app = require("./app");
 const path = require("path");
 const mongo = require("./app/core/db/mongo");
@@ -12,9 +17,3 @@ const server = app.listen(process.env.PORT, () => {
 socket.initialize(server);
 mongo.startDatabase();
 // redis.startDatabase();
-
-process.on("uncaughtException", function (err) {
-  let time = new Date().toString();
-  err = {...err, time};
-  console.trace(err.stack);
-});
