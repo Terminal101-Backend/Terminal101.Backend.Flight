1ad505d91976b9e8691bd923f66a707facca5383
Samir <mail6410@gmail.com>
2022-09-13 12:50:18 +0430

    Update error logs

--- app/helpers/responseHelper.js
+++ app/helpers/responseHelper.js
@@ -14,7 +14,7 @@ exports.success = (res, data) => {
 
 //making response for error by status code (default is 404)
 exports.error = (res, message, statusCode = 404, data = []) => {
-  console.error(`Code: ${statusCode}, Message: ${message}`);
+  console.trace(`Code: ${statusCode}, Message: ${message}`);
   return res.status(statusCode).send(
     {
       status: false,
@@ -49,7 +49,7 @@ exports.exception = (res, error) => {
       data,
     });
   }
-  console.error(`Code: 500, Message: ${message}`);
+  console.trace(`Code: 500, Message: ${message}`);
   return res.status(500).send({
     status: false,
     message: `{{${message}}}`,
