00ed8a5997d1182c9a35648b38223f4632324473
Samir <mail6410@gmail.com>
2022-09-13 14:39:45 +0430

    Update filter structure

--- app/helpers/filterHelper.js
+++ app/helpers/filterHelper.js
@@ -1,22 +1,75 @@
 /**
- * 
- * @param {Aggregate} aggregate 
- * @param {Object} filters 
- * @param {String} sort 
+ *
+ * @param {Aggregate} aggregate
+ * @param {Object} filters
+ * @param {String} sort
  * @returns {Aggregate}
  */
 module.exports.filterAndSort = async (aggregate, filters, sort) => {
+  let objFilters = {};
+
   if (!!sort) {
     if (sort[0] === "-") {
-      sort = { [sort.substring(1)]: -1 };
+      sort = {[sort.substring(1)]: -1};
     } else {
-      sort = { sort: 1 };
+      sort = {sort: 1};
     }
   }
 
+  Object.entries((filters ?? {})).forEach(([key, value]) => {
+    const keys = key.split("|");
+    const values = value.split("|");
+
+    objFilters = {
+      ...objFilters,
+      $or: [...keys.reduce((res, cur) => {
+        if (cur.endsWith("!")) {
+          cur = cur.substring(0, cur.length - 1);
+
+        } else if (cur.endsWith("<")) {
+          cur = cur.substring(0, cur.length - 1);
+
+        } else if (cur.endsWith("<=")) {
+          cur = cur.substring(0, cur.length - 2);
+
+        } else if (cur.endsWith(">")) {
+          cur = cur.substring(0, cur.length - 1);
+
+        } else if (cur.endsWith(">=")) {
+          cur = cur.substring(0, cur.length - 2);
+
+        } else {
+
+        }
+        switch (cur.substring(cur.length - 1)) {
+          case "!":
+            break;
+
+          case "<":
+            cur = cur.substring(0, cur.length - 1);
+            break;
+
+          case "<=":
+            cur = cur.substring(0, cur.length - 2);
+            break;
+
+          case ">":
+            cur = cur.substring(0, cur.length - 1);
+            break;
+
+          case ">=":
+            cur = cur.substring(0, cur.length - 2);
+            break;
+
+          default:
+        }
+      }, [])],
+    }
+  });
+
   aggregate.append({
     $match: {
-      ...filters
+      ...objFilters
     }
   });
   if (!!sort) {
