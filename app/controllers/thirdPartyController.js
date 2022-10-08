e0e0653d8b95f7ee9200ebc6f0879ca7757b57df
Samir <mail6410@gmail.com>
2022-10-08 10:34:00 +0000

    Check providers

--- app/controllers/thirdPartyController.js
+++ app/controllers/thirdPartyController.js
@@ -3,7 +3,7 @@ const request = require("../helpers/requestHelper");
 const { providerRepository } = require("../repositories");
 const { EProvider } = require("../constants");
 const { amadeus, parto, avtra, accountManagement } = require("../services");
-const { tokenHelper } = require("../helpers");
+const { tokenHelper, avtraHelper } = require("../helpers");
 
 // NOTE: Search flights by provider
 module.exports.lowFareSearch = async (req, res) => {
@@ -12,32 +12,44 @@ module.exports.lowFareSearch = async (req, res) => {
     const testMode = req.params[0] === "/test";
 
     const { data: availableProviders } = await accountManagement.getThirdPartyUserAvailableProviders(decodedToken.owner, decodedToken.user);
+    let result = {
+      flightDetails: [],
+    };
 
-    availableProviders.forEach(provider => {
-      console.log(provider);
-    });
+    for (const provider of availableProviders) {
+      switch (provider) {
+        case "AVTRA":
+          const providerResult = await avtraHelper.searchFlights(req.query);
 
-    const result = await avtra.lowFareSearch(
-      req.query.originLocationCode,
-      req.query.destinationLocationCode,
-      req.query.departureDate,
-      req.query.returnDate,
-      req.query.segments,
-      req.query.adults,
-      req.query.children,
-      req.query.infants,
-      req.query.travelClass,
-      req.query.includedAirlineCodes,
-      req.query.excludedAirlineCodes,
-      req.query.nonStop,
-      req.query.currencyCode,
-      testMode);
+          result.origin = providerResult.origin;
+          result.destination = providerResult.destination;
+          result.flightDetails.push(...providerResult.flightDetails);
+      }
+    };
 
-    if (!!result.success) {
-      response.success(res, result.data);
-    } else {
-      throw "Provider error " + result.error.code + ": " + result.error.message;
-    }
+    response.success(res, result);
+
+    // const result = await avtra.lowFareSearch(
+    //   req.query.originLocationCode,
+    //   req.query.destinationLocationCode,
+    //   req.query.departureDate,
+    //   req.query.returnDate,
+    //   req.query.segments,
+    //   req.query.adults,
+    //   req.query.children,
+    //   req.query.infants,
+    //   req.query.travelClass,
+    //   req.query.includedAirlineCodes,
+    //   req.query.excludedAirlineCodes,
+    //   req.query.nonStop,
+    //   req.query.currencyCode,
+    //   testMode);
+
+    // if (!!result.success) {
+    //   response.success(res, result.data);
+    // } else {
+    //   throw "Provider error " + result.error.code + ": " + result.error.message;
+    // }
   } catch (e) {
     response.exception(res, e);
   }
