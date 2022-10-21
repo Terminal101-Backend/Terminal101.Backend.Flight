1dbf9e0745713613f5b989a94c54764178b5159d
Samir <mail6410@gmail.com>
2022-10-21 21:54:47 +0000

    Update commission routes and controllers and validators

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,4 +3,5 @@ module.exports.bookFlightController = require("./bookFlightController");
 module.exports.flightTicketController = require("./flightTicketController");
 module.exports.providerController = require("./providerController");
 module.exports.flightConditionController = require("./flightConditionController");
+module.exports.commissionController = require("./commissionController");
 module.exports.thirdPartyController = require("./thirdPartyController");
--- app/validations/index.js
+++ app/validations/index.js
@@ -3,4 +3,5 @@ module.exports.bookFlightValidator = require("./bookFlightValidator");
 module.exports.flightTicketValidator = require("./flightTicketValidator");
 module.exports.providerValidator = require("./providerValidator");
 module.exports.flightConditionValidator = require("./flightConditionValidator");
+module.exports.commissionValidator = require("./commissionValidator");
 module.exports.thirdPartyValidator = require("./thirdPartyValidator");
